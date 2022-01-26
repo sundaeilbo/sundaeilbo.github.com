4d35280f698983fcbbac42fbba49972af5253a76
sundaeilbo <89733704+sundaeilbo@users.noreply.github.com>
2022-01-26 12:11:12 +0900

    Update scripts.js

--- _js/scripts.js
+++ _js/scripts.js
@@ -242,7 +242,7 @@ $( "#contactForm" ).submit( function( e ) {
     $.ajax( {
 
         // Change the email address here:
-        url: "https://formspree.io/jan.czizikow@Fmail.com",
+        url: "https://formspree.io/g_gimbab@naver.com",
         method: "POST",
         data: $( this ).serialize(),
         dataType: "json",
